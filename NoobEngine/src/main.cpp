6a9f3739f94283cb7e4de1f2b669ce83514eaa52
dylanhan99 <dylanhan99@gmail.com>
2022-12-02 23:33:05 +0800

    Added dependencies GLEW, GLFW, GLM

--- NoobEngine/src/main.cpp
+++ NoobEngine/src/main.cpp
@@ -1,7 +1,19 @@
+#define GLFW_INCLUDE_NONE
+
 #include <stdio.h>
+#include <glfw3.h>
+#include <glew.h>
+#include <glm.hpp>
 
 
 int main() {
+	if (!glfwInit()) {
+		printf("GLFW init fail\n");
+		glfwTerminate();
+	}
+	glTexCoord1d(5.0);
+	glm::abs(10);
 	printf("Hello Uwuorld\n");
+	glfwTerminate();
 	return 0;
 }
