baf0824f0f49b4154b5e8f8a612ddd2762720bda
dylanhan99 <dylanhan99@gmail.com>
2022-12-07 23:11:05 +0800

    Buffers and Bufferlayout setup

---
+++ NoobEngine/inc/Buffer.h
@@ -0,0 +1,98 @@
+/**
+ * @file	Buffer.h
+ * @author	Han Wei, Dylan (dylanhan99@gmail.com)
+ * @version 1.0
+ * @date	07/12/2022
+ * @brief	This file contains structures and logic pertaining to 
+ *			setting up VAO, VBO and IBO.
+ *			As a dynamic draw, the BufferLayout object holds all
+ *			the vertices of the current batch.
+ *
+ */
+
+#pragma once
+#include <vector>
+#include <glad.h>
+#include "Log.h"
+
+namespace NoobEngine { namespace Graphics {
+
+struct BufferElement
+{
+	unsigned int type;
+	unsigned int count;
+	unsigned char normalized;
+
+	static unsigned int GetSizeOfType(unsigned int _type)
+	{
+		switch (_type)
+		{
+		case GL_FLOAT:			return 4;
+		case GL_UNSIGNED_INT:	return 4;
+		case GL_UNSIGNED_BYTE:	return 1;
+		}
+		MY_ASSERT(false);
+		return 0;
+	}
+};
+
+class BufferLayout
+{
+private:
+	std::vector<BufferElement> m_Elements;
+	unsigned int m_Stride;
+public:
+	BufferLayout();
+	~BufferLayout();
+
+	template<typename T>
+	void Push(unsigned int _count) 
+	{
+		MY_ASSERT(false);
+	}
+
+	inline const std::vector<BufferElement>& GetElements()	const { return m_Elements; }
+	inline		 unsigned int				 GetStride()	const { return m_Stride; }
+};
+
+class VertexBuffer
+{
+private:
+	unsigned int m_RendererID;
+public:
+	VertexBuffer(unsigned int _size);
+	~VertexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+};
+
+class VertexArray 
+{
+private:
+	unsigned int m_RendererID;
+public:
+	VertexArray();
+	~VertexArray();
+
+	void AddBuffer(const VertexBuffer& _vbo, const BufferLayout& _layout);
+	void Bind() const;
+	void Unbind() const;
+};
+
+class IndexBuffer
+{
+private:
+	unsigned int m_RendererID;
+	unsigned int m_Count;
+public:
+	IndexBuffer(const unsigned int *_data, unsigned int _count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_Count; }
+};
+
+}}
---
+++ NoobEngine/src/buffer.cpp
@@ -0,0 +1,125 @@
+#include "Buffer.h"
+
+namespace NoobEngine { namespace Graphics {
+
+	/**
+	 * Buffer Layout
+	 */
+
+	BufferLayout::BufferLayout()
+		: m_Stride(0)
+	{
+
+	}
+
+	BufferLayout::~BufferLayout()
+	{
+
+	}
+
+	template<>
+	void BufferLayout::Push<float>(unsigned int _count)
+	{
+		m_Elements.push_back({ GL_FLOAT, _count, GL_FALSE });
+		m_Stride += _count * BufferElement::GetSizeOfType(GL_FLOAT);
+	}
+
+	template<>
+	void BufferLayout::Push<unsigned int>(unsigned int _count)
+	{
+		m_Elements.push_back({ GL_UNSIGNED_INT, _count, GL_FALSE });
+		m_Stride += _count * BufferElement::GetSizeOfType(GL_UNSIGNED_INT);
+	}
+
+	/**
+	 * Vertex Array
+	 */
+	
+	VertexArray::VertexArray()
+	{
+		glGenVertexArrays(1, &m_RendererID);
+	}
+
+	VertexArray::~VertexArray()
+	{
+		glDeleteVertexArrays(1, &m_RendererID);
+	}
+
+	void VertexArray::AddBuffer(const VertexBuffer& _vbo, const BufferLayout& _layout)
+	{
+		Bind();
+		_vbo.Bind();
+
+		const auto& elements = _layout.GetElements();
+		for (unsigned int i = 0, offset = 0; i < elements.size(); ++i) {
+			const auto& element = elements[i];
+			glVertexAttribPointer(i, element.count, element.type, element.normalized, 
+				_layout.GetStride(), (const GLvoid*)offset);
+			offset += element.count * BufferElement::GetSizeOfType(element.type);
+		}
+	}
+
+	void VertexArray::Bind() const
+	{
+		glBindVertexArray(m_RendererID);
+	}
+
+	void VertexArray::Unbind() const
+	{
+		glBindVertexArray(0);
+	}
+
+	/**
+	 * Vertex Buffer
+	 */
+
+	VertexBuffer::VertexBuffer(unsigned int _size)
+	{
+		glGenBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ARRAY_BUFFER, _size, nullptr, GL_DYNAMIC_DRAW);
+	}
+
+	VertexBuffer::~VertexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void VertexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void VertexBuffer::Unbind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+	
+	/**
+	 * Index Buffer
+	 */
+
+	IndexBuffer::IndexBuffer(const unsigned int* _data, unsigned int _count)
+		: m_Count(_count)
+	{
+		glGenBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, _count * sizeof(unsigned int), _data, GL_STATIC_DRAW);
+	}
+
+	IndexBuffer::~IndexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void IndexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void IndexBuffer::Unbind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+}}
