18f7873104a7a8999e500d081623dd4c9e461d51
dylanhan99 <dylanhan99@gmail.com>
2022-12-10 23:32:13 +0800

    Added glad.h

---
+++ NoobEngine/NoobEngine/src/nepch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#include <glad.h>
+#include <glm.hpp>
+
+#include "Log/Log.h"
